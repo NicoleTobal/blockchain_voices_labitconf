57068e2652517e08e5bc38cf15123bdbc3e9d2f4
Nicole Tobal <nickytobal@gmail.com>
2018-12-06 17:21:34 -0300

    FIX

--- src/FileComponents/FileInput.jsx
+++ src/FileComponents/FileInput.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { onFileUpload } from '../utils/fileHelper';
-import loading from './loading.gif';
 import ReactLoading from 'react-loading';
 
 //import { onFileUpload } from '../utils/fileHelper';
