0f1d30eaf9dbfcc84e8f0ae675f7d845e1f5bb46
Nicole Tobal <nickytobal@gmail.com>
2018-12-07 12:25:20 -0300

    Static videos added

--- src/FileComponents/Highlights.jsx
+++ src/FileComponents/Highlights.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
+import video from './01\ NOV\ 2018.mov';
+import video2 from './02\ NOV\ 2018-2.mov';
 
 class Highlights extends Component {
 
@@ -16,20 +18,15 @@ class Highlights extends Component {
         <div className="container-fluid container-highlights">
           <div className="row" id="highlights-videos-1">
             <div className="col-md-6 embed-responsive embed-responsive-16by9" id="highlights-center-big">
-              <iframe title="0" src="https://www.youtube-nocookie.com/embed/SSo_EIwHSd4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              <video style={{padding: '5px'}} width="540" height="310" controls controlsList="nodownload">
+                <source src={video} type="video/mp4" />
+              </video>
             </div>
             <div className="col-md-6 embed-responsive embed-responsive-16by9" id="highlights-center-big">
-              <iframe title="1" src="https://www.youtube-nocookie.com/embed/MhLdsJo_IRM" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              <video style={{padding: '5px'}} width="540" height="310" controls controlsList="nodownload">
+                <source src={video2} type="video/mp4" />
+              </video>
             </div>
-          </div>
-        </div>
-
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-6 col-md-4 embed-responsive embed-responsive-16by9" id="highlights-center-big"><iframe src="https://www.youtube-nocookie.com/embed/SSo_EIwHSd4" title="2" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-            <div className="col-6 col-md-4 embed-responsive embed-responsive-16by9" id="highlights-center-big"><iframe src="https://www.youtube-nocookie.com/embed/MhLdsJo_IRM" title="3" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-            <div className="col-6 col-md-4 embed-responsive embed-responsive-16by9" id="highlights-center-big"><iframe src="https://www.youtube-nocookie.com/embed/MhLdsJo_IRM" title="4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
-            <div className="col-6 col-md-4 embed-responsive embed-responsive-16by9" id="highlights-center-big"><iframe src="https://www.youtube-nocookie.com/embed/MhLdsJo_IRM" title="4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></div>
           </div>
         </div>
       </div>
